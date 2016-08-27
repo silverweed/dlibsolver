b771cc2d7132c5986d66f9b2311d2fe297e590e7
silverweed <silverweed1991@gmail.com>
2016-08-27 13:57:39 +0200

    constant vars are now static immutable fields

--- src/solver.d
+++ src/solver.d
@@ -63,29 +63,31 @@ class RK2Solver : Solver {
 		for (int i = 0; i < k.length; ++i)
 			k[i] = new real[](dim);
 
-		auto t_coef = [0, 0.5];
-		real[2][2] x_coefs;
-		x_coefs[1][0] = 0.5;
-		auto weights = [0, 1];
-		
 		for (int i = 0; i < 2; ++i) {
 			for (int j = 0; j < dim; ++j) {
 				PosVec midPoint = mX.dup;
 				for (int l = i - 1; l >= 0; --l)
 					for (int m = 0; m < dim; ++m)
-						midPoint[m] += x_coefs[i][l] * k[l][m];
-				k[i][j] = step * mF[j](mt + t_coef[i] * step, midPoint);
+						midPoint[m] += X_COEFS[i][l] * k[l][m];
+				k[i][j] = step * mF[j](mt + T_COEF[i] * step, midPoint);
 			}
 		}
 
 		// evaluate new position
 		for (int i = 0; i < dim; ++i)
-			x[i] += weights[0] * k[0][i] + weights[1] * k[1][i];
+			x[i] += WEIGHTS[0] * k[0][i] + WEIGHTS[1] * k[1][i];
 
 		mX = x;
 		mt += step;
 		return mX;
 	}
+
+	private static immutable T_COEF = [0, 0.5];
+	private static immutable WEIGHTS = [0, 1];
+	private static immutable X_COEFS = [
+		[0.,  0],
+		[0.5, 0]
+	];
 }
 
 class RK4Solver : Solver {
@@ -98,37 +100,36 @@ class RK4Solver : Solver {
 		PosVec x = mX;
 		auto dim = mX.length;
 
-
 		auto k = new real[][4];
 		for (int i = 0; i < k.length; ++i)
 			k[i] = new real[](dim);
-		auto t_coef = [0, 1/3., 2/3., 1];
-		real[4][4] x_coefs;
-		x_coefs[1][0] = 1./3.;
-		x_coefs[2][1] = 1.;
-		x_coefs[2][0] = -1./3.;
-		x_coefs[3][2] = 1.;
-		x_coefs[3][1] = -1.;
-		x_coefs[3][0] = 1.;
-		auto weights = [1/8., 3/8., 3/8., 1/8.];
-		
+
 		for (int i = 0; i < 4; ++i) {
 			for (int j = 0; j < dim; ++j) {
 				PosVec midPoint = mX.dup;
 				for (int l = i - 1; l >= 0; --l)
 					for (int m = 0; m < dim; ++m)
-						midPoint[m] += x_coefs[i][l] * k[l][m];
-				k[i][j] = step * mF[j](mt + t_coef[i] * step, midPoint);
+						midPoint[m] += X_COEFS[i][l] * k[l][m];
+				k[i][j] = step * mF[j](mt + T_COEF[i] * step, midPoint);
 			}
 		}
 
 		// evaluate new position
 		for(int i = 0; i < 4; ++i)
 			for(int j = 0; j < dim; ++j)
-				x[j] += weights[i] * k[i][j];
+				x[j] += WEIGHTS[i] * k[i][j];
 
 		mX = x;
 		mt += step;
 		return mX;
 	}
+
+	private static immutable T_COEF = [0, 1/3., 2/3., 1];
+	private static immutable WEIGHTS = [1/8., 3/8., 3/8., 1/8.];
+	private static immutable X_COEFS = [
+		[0.,    0, 0, 0],
+		[1/3.,  0, 0, 0],
+		[-1/3., 1, 0, 0],
+		[1.,   -1, 1, 0]
+	];
 }
